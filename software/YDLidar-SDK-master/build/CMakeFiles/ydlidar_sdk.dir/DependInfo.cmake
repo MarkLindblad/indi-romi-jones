9c4f04b3ec1d2779e77c524e5205a74a1d5eec9f
BassemHalim <bassemamir459@gmail.com>
2021-11-30 20:58:20 +0000

    reading and sending data done

---
+++ software/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/core/serial/impl/unix/lock.c" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/core/serial/impl/unix/lock.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../core"
+  "../src"
+  "../"
+  "."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/core/base/timer.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/core/base/timer.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/core/common/ydlidar_def.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/core/common/ydlidar_def.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/core/network/ActiveSocket.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/core/network/ActiveSocket.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/core/network/PassiveSocket.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/core/network/PassiveSocket.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/core/network/SimpleSocket.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/core/network/SimpleSocket.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/core/serial/impl/unix/list_ports_linux.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/core/serial/impl/unix/list_ports_linux.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/core/serial/impl/unix/unix_serial.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/core/serial/impl/unix/unix_serial.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/core/serial/serial.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/core/serial/serial.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/src/CYdLidar.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/src/CYdLidar.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/src/ETLidarDriver.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/src/ETLidarDriver.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/src/ydlidar_driver.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/src/ydlidar_driver.cpp.o"
+  "/home/pi/Desktop/EECS149/YDLidar-SDK-master/src/ydlidar_sdk.cpp" "/home/pi/Desktop/EECS149/YDLidar-SDK-master/build/CMakeFiles/ydlidar_sdk.dir/src/ydlidar_sdk.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../core"
+  "../src"
+  "../"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ software/YDLidar-SDK-master/samples/lidar_c_api_test.c
@@ -0,0 +1,164 @@
+﻿
+
+//socket includes
+#include <netinet/in.h>
+#include <sys/socket.h>
+#define PORT 8080
+
+//-------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <string.h>
+#include "ydlidar_sdk.h"
+#if defined(_MSC_VER)
+#pragma comment(lib, "ydlidar_sdk.lib")
+#endif
+
+int main(int argc, const char *argv[]) {
+    
+    //--------------socket------------------------
+   int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+       
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+       
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, 
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    
+    //--------------------------------------------------
+
+
+    os_init();
+    YDLidar *laser = lidarCreate();
+    //string prop
+    char port[50] = "/dev/ydlidar";
+    LidarPort ports;
+    int size = lidarPortList(&ports);
+    int i = 0;
+    for(i =0; i < size; i++) {
+        printf("port: %s\n", ports.port[i].data);
+        /// last port
+        strcpy(port, ports.port[i].data);
+    }
+    setlidaropt(laser, LidarPropSerialPort, port, sizeof(port));
+    strcpy(port, "");
+    setlidaropt(laser, LidarPropIgnoreArray,port, sizeof(port));
+
+    //int prop
+    int i_optvalue = 115200;
+    setlidaropt(laser, LidarPropSerialBaudrate, &i_optvalue, sizeof(int));
+    // i_optvalue = TYPE_TOF;
+    i_optvalue = TYPE_TRIANGLE;
+    setlidaropt(laser, LidarPropLidarType, &i_optvalue, sizeof(int));
+    i_optvalue = YDLIDAR_TYPE_SERIAL;
+    setlidaropt(laser, LidarPropDeviceType, &i_optvalue, sizeof(int));
+    i_optvalue = 3;
+    setlidaropt(laser, LidarPropSampleRate, &i_optvalue, sizeof(int));
+
+    //bool prop
+    bool b_optval = true;
+    setlidaropt(laser, LidarPropAutoReconnect, &b_optval, sizeof(bool));
+    b_optval = true;
+    setlidaropt(laser, LidarPropSingleChannel, &b_optval, sizeof(bool));
+    b_optval = false;
+    setlidaropt(laser, LidarPropIntenstiy, &b_optval, sizeof(bool));
+    setlidaropt(laser, LidarPropInverted, &b_optval, sizeof(bool));
+    setlidaropt(laser, LidarPropReversion, &b_optval, sizeof(bool));
+    b_optval = true;
+    setlidaropt(laser, LidarPropSupportMotorDtrCtrl, &b_optval, sizeof(bool));
+    b_optval = false;
+    setlidaropt(laser, LidarPropFixedResolution, &b_optval, sizeof(bool));
+
+    //float prop
+    float f_optval = 4.f;
+    setlidaropt(laser, LidarPropScanFrequency, &f_optval, sizeof(float));
+    f_optval = 360.0f;
+    setlidaropt(laser, LidarPropMaxAngle, &f_optval, sizeof(float));
+    f_optval = 0.0f;
+    setlidaropt(laser, LidarPropMinAngle, &f_optval, sizeof(float));
+    // f_optval = 8.f;
+    f_optval = 10.f;
+    setlidaropt(laser, LidarPropMaxRange, &f_optval, sizeof(float));
+    // f_optval = 0.12f;
+    f_optval = 0.12f;
+    setlidaropt(laser, LidarPropMinRange, &f_optval, sizeof(float));
+
+    getlidaropt(laser, LidarPropSerialBaudrate,&i_optvalue, sizeof(int));
+    printf("baudrate: %d\n", i_optvalue);
+
+    bool ret = initialize(laser);
+   if(ret) {
+       ret = turnOn(laser);
+   }
+
+    LaserFan scan;
+    LaserFanInit(&scan);
+
+    
+    float point[2] = {0};
+    while (ret && os_isOk()) {
+        if(doProcessSimple(laser, &scan)) {
+          
+;
+           for (int i = 0; i < scan.npoints; i++){
+		        fprintf(stdout, "distance %f angle %.4f\n", scan.points[i].range*100, scan.points[i].angle * 57.29);
+                point[0] = scan.points[i].range*100;
+                point[1] = scan.points[i].angle * 57.29;
+                // sprintf (msg, "%.0f, %.0f",scan.points[i].range*100, scan.points[i].angle * 57.29 );
+                send(new_socket , point , 2 * sizeof(float) , 0 );
+                fflush(stdout);
+           }
+
+	       	
+        } else {
+            fprintf(stderr, "Failed to get Lidar Data\n");
+            fflush(stderr);
+        }
+    }
+    LaserFanDestroy(&scan);
+    turnOff(laser);
+    disconnecting(laser);
+    lidarDestroy(&laser);
+    return 0;
+
+}

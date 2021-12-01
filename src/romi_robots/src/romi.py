fa64c9289cbe3336c0528e7d733a2212d632138f
William Catzin <williamcatzin@berkeley.edu>
2021-12-01 13:37:00 -0800

    created romi_pub wrapper class for romi robots to publish sensor data.

--- src/romi_robots/src/romi.py
+++ src/romi_robots/src/romi.py
@@ -6,69 +6,60 @@
 
 #Import the rospy package. For an import to work, it must be specified
 #in both the package manifest AND the Python file in which it is used.
-from re import S
 import rospy
-import tf2_py
-import tf2_ros
-import tf2_msgs.msg
 import sys
-import numpy as np
-from sensor_msgs.msg import LaserScan
-import geometry_msgs
-import asyncio
-from bleak import BleakClient
 from romi_pub import Romi
+# Import socket module to read sensor data
+# from romi robots via WIFI
+import socket            
+import struct 
 
+
+# Port on which you want to connect
+_PORT = 8080
+# IP Address
+_IP_ADDRESS = '192.168.137.22' 
+
+# TODO Romi() requires sensor data
 def main(args):
+    # get romi name
     romi_name = args[1]
+    # Create a socket object
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connect to the server on local computer
+    sock.connect((_IP_ADDRESS, _PORT))
+    # initialize ROS node 
     rospy.init_node(romi_name, anonymous=True)
-    romi = Romi()
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
+    # instantiate romi object
+    romi = Romi(romi_name)
+    while True:
+        # Receive sensor data
+        range_data = struct.unpack('f', sock.recv(4))[0] 
+        angle_data = struct.unpack('f', sock.recv(4))[0]
+        time_data = struct.unpack('f', sock.recv(4))[0]
+        left_tick_data = struct.unpack('f', sock.recv(4))[0]
+        right_tick_data = struct.unpack('f', sock.recv(4))[0]
+        gyro_data = struct.unpack('f', sock.recv(4))[0]
+        # Print for debugging
+        print("Range: \n", range_data)
+        print("Angle: \n", angle_data)
+        print("Left Ticks : \n", left_tick_data)
+        print("Right Ticks: ", right_tick_data)
+        print("Gyro Data: \n", gyro_data)
+        # publish laser data
+        romi.publish_laser(range_data, angle_data, time_data)
+        # publish odom data
+        romi.broadcast_and_publish_odom(left_tick_data, right_tick_data, gyro_data)
+        
 
 if __name__ == '__main__':
     # sys.argv contains romi info
     main(sys.argv)
 
 
-# THIS CODE IS FOR BLE COMMUNICATION WITH THE ROMI ROBOTS
-#####################################################################
-# from ble_utils import parse_ble_args, handle_sigint, LAB11
-# args = parse_ble_args('Print advertisement data from a BLE device')
-# addr = args.addr.lower()
-# timeout = args.timeout
-# handle_sigint()
+       
+ 
+            
+ 
 
-
-# ROMI1_ADDRESS = hex(0x1234)
-
-# async def main(address):
-#     print(f"searching for device {address} ({timeout}s timeout)")
-#     async with BleakClient(address,timeout=timeout) as client:
-#         print(f"Connected: {client.is_connected}")        
-#         for service in client.services:
-#             print(f"[Service] {service}")
-#             for char in service.characteristics:
-#                 if "read" in char.properties:
-#                     try:
-#                         value = bytes(await client.read_gatt_char(char.uuid))
-#                         print(f"\t[Characteristic] {char} ({','.join(char.properties)}), Value: {value}")
-#                     except Exception as e:
-#                         print(f"\t[Characteristic] {char} ({','.join(char.properties)}), Value: {e}")
-#                 else:
-#                     value = None
-#                     print(f"\t[Characteristic] {char} ({','.join(char.properties)}), Value: {value}")
-
-#                 for descriptor in char.descriptors:
-#                     try:
-#                         value = bytes(await client.read_gatt_descriptor(descriptor.handle))
-#                         print(f"\t\t[Descriptor] {descriptor}) | Value: {value}")
-#                     except Exception as e:
-#                         print(f"\t\t[Descriptor] {descriptor}) | Value: {e}")
-
-
-# if __name__ == "__main__":
-#     while True:
-#         asyncio.run(main(ROMI1_ADDRESS))
+ 
--- src/romi_robots/src/romi_pub.py
+++ src/romi_robots/src/romi_pub.py
@@ -5,31 +5,56 @@
 #
 ################################################################################
 
+from _typeshed import Self
+from numpy.lib.function_base import angle
 import rospy
-import tf2_py
-import tf2_ros
-import tf2_msgs.msg
-import sys
+import tf
 import numpy as np
 from sensor_msgs.msg import LaserScan
+from nav_msgs import Odometry
 import geometry_msgs
 from romi import Romi
 
-class Romi():
-    def __init__(self, romi_name, laser_topic, odom_topic):
-        self.name = romi_name
+# YDLIDAR SPECIFICATIONS
+ANGLE_MIN = 0.0
+ANGLE_MAX = 360.0
+RANGE_MIN = 0.0
+RANGE_MAX = 0.0
 
-    
-    def publish(range_data, angle_data, wheel_encoder, gyro):
-        laserScan_pub = rospy.Publisher('laser_point', LaserScan, queue_size=10)
-        while not rospy.is_shutdown():
-            current_time = rospy.Time.now()
-            scan = LaserScan()
-            scan.header.stamp = current_time
-            scan.header.frame_id = 'laser_frame'
-            scan.angle_min = 0.0
-            scan.angle_max - 0.0
-            scan.angle_increment = 0.0
-            scan.time_increment = 0.0
-            scan.range_min = 0.0
-            scan.range_max = 0.0
+# ROMI ROBOT PHYSICAL SPECIFICATIONS
+WHEEL_TRACK = 0.0
+WHEEL_RADIUS = 0.0
+TPR = 351
+
+class Romi():
+    def __init__(self, name):
+        # romi's name
+        self.name = name
+        # romi's old wheel encoder data
+        self.old_left_ticks = 0.0
+        self.old_right_ticks = 0.0
+        # broadcasting and publishing objects
+        self.laser_pub = rospy.Publisher("laser", LaserScan, queue_size=10)
+        self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=10)
+        self.odom_broadcaster = tf.TransformBroadcaster()
+
+    def publish_laser(self, range, angle, time_stamp):
+        # Publish to laser topic
+        scan = LaserScan()
+        scan.header.stamp = time_stamp
+        scan.header.frame_id = 'laser_frame'
+        scan.angle_min = ANGLE_MIN
+        scan.angle_max - ANGLE_MAX
+        scan.angle_increment = angle
+        scan.time_increment = 0.0
+        scan.range_min = RANGE_MIN
+        scan.range_max = RANGE_MAX
+            
+
+    def broadcast_and_publish_odom(self, left_tick_data, right_tick_data):
+        change_in_left_ticks = left_tick_data - self.old_left_ticks
+        cahnge_in_right_ticks = right_tick_data - self.old_right_ticks
+
+        
+
+            

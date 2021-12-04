71115d5e6a31a3bb6e8182dbcd3f3d1c88519bb0
BassemHalim <bassemamir459@gmail.com>
2021-12-04 00:54:59 +0000

    .

--- software/main.c
+++ software/main.c
@@ -39,47 +39,47 @@ int main(int argc, const char *argv[])
 {
     
     //--------------socket------------------------
-    //    int server_fd, new_socket, valread;
-    //     struct sockaddr_in address;
-    //     int opt = 1;
-    //     int addrlen = sizeof(address);
-    //    // perror("started");
-    //     // Creating socket file descriptor
-    //     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    //     {
-    //         perror("socket failed");
-    //         exit(EXIT_FAILURE);
-    //     }
+       int server_fd, new_socket, valread;
+        struct sockaddr_in address;
+        int opt = 1;
+        int addrlen = sizeof(address);
+       // perror("started");
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
 
-    //     // Forcefully attaching socket to the port 8080
-    //     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-    //                                                   &opt, sizeof(opt)))
-    //     {
-    //         perror("setsockopt");
-    //         exit(EXIT_FAILURE);
-    //     }
-    //     address.sin_family = AF_INET;
-    //     address.sin_addr.s_addr = INADDR_ANY;
-    //     address.sin_port = htons( PORT );
-    //        printf("here");
-    //     // Forcefully attaching socket to the port 8080
-    //     if (bind(server_fd, (struct sockaddr *)&address,
-    //                                  sizeof(address))<0)
-    //     {
-    //         perror("bind failed");
-    //         exit(EXIT_FAILURE);
-    //     }
-    //     if (listen(server_fd, 3) < 0)
-    //     {
-    //         perror("listen");
-    //         exit(EXIT_FAILURE);
-    //     }
-    //     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-    //                        (socklen_t*)&addrlen))<0)
-    //     {
-    //         perror("accept");
-    //         exit(EXIT_FAILURE);
-    //     }
+        // Forcefully attaching socket to the port 8080
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                      &opt, sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons( PORT );
+           printf("here");
+        // Forcefully attaching socket to the port 8080
+        if (bind(server_fd, (struct sockaddr *)&address,
+                                     sizeof(address))<0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                           (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
 
         //------------LIDAR init--------------------
 
@@ -149,7 +149,11 @@ int main(int argc, const char *argv[])
         LaserFan scan;
         LaserFanInit(&scan);
 
-
+    // typedef enum{
+    //     scanning,
+    //     sending,
+    //     drive,
+    // }state;
 
 
     printf("started\n");
@@ -160,11 +164,7 @@ int main(int argc, const char *argv[])
     sensors.leftWheelEncoder = 0;
     sensors.rightWheelEncoder = 0;
 
-    // while(1){
-    
-    // kobukiSensorPoll(&sensors);
-    // printf("wheel ticks: %d\n", sensors.leftWheelEncoder);
-    // }
+
     kobukiUARTUnInit();
         while (ret && os_isOk()) {
             if(doProcessSimple(laser, &scan)) {
@@ -174,13 +174,14 @@ int main(int argc, const char *argv[])
                 point[3] = sensors.leftWheelEncoder;
                 point [4] = sensors.rightWheelEncoder;
                for (int i = 0; i < scan.npoints; i++){
+                    printf("scans: %d\n", scan.npoints);
     		        // fprintf(stdout, "distance %f angle %.4f\n", scan.points[i].range*100, scan.points[i].angle * 57.29);
                     point[1] = scan.points[i].range*100;
                     point[2] = scan.points[i].angle * 57.29;
                     printf("stamp: %d distance: %.2f angle: %.2f ticks (%d, %d)\n", point[0], point[1], point[2],
                                                                                              point[3], point[4]);
                     // sprintf (msg, "%.0f, %.0f",scan.points[i].range*100, scan.points[i].angle * 57.29 );
-                    // send(new_socket , point , 2 * sizeof(float) , 0 );
+                    send(new_socket , point , 5 * sizeof(float) , 0 );
                     fflush(stdout);
                }
 

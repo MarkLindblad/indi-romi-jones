ae085da2366fa8ec85b04e29552045596fb32136
Bassem Halim <bassemamir459@gmail.com>
2021-12-04 21:48:28 -0800

    modified while loop

--- software/main.c
+++ software/main.c
@@ -154,7 +154,7 @@ int main(int argc, const char *argv[])
 
     printf("started\n");
     kobukiUARTInit();
-    kobukiDriveDirect(30, 30);
+    kobukiDriveDirect(40, 40);
     kobukiUARTInit();
     KobukiSensors_t sensors;
     sensors.leftWheelEncoder = 0;
@@ -175,7 +175,7 @@ int main(int argc, const char *argv[])
                 point [4] = sensors.rightWheelEncoder;
                for (int i = 0; i < scan.npoints; i++){
     		        // fprintf(stdout, "distance %f angle %.4f\n", scan.points[i].range*100, scan.points[i].angle * 57.29);
-                    point[1] = scan.points[i].range*100;
+                    point[1] = scan.points[i].range;
                     point[2] = scan.points[i].angle * 57.29;
                     printf("stamp: %d distance: %.2f angle: %.2f ticks (%d, %d)\n", point[0], point[1], point[2],
                                                                                              point[3], point[4]);
@@ -189,6 +189,7 @@ int main(int argc, const char *argv[])
                 fflush(stderr);
             }
         }
+        kobukiDriveDirect(0,0);
         LaserFanDestroy(&scan);
         turnOff(laser);
         disconnecting(laser);

154c787a798eb36664dc78990cdba681671a0af8
Bassem Halim <bassemamir459@gmail.com>
2021-12-09 11:24:01 -0800

    added avg in 4 directions

--- software/main.c
+++ software/main.c
@@ -35,6 +35,35 @@
 // #include <unistd.h>
 #define SEND true
 
+// get the avg distance in 4 directions, out => [-1, 0, 90, 180, 270] 
+//angles are in radian and range from -180 to 180
+//didn't convert angles on the fly to save time
+void getAvg(LaserFan *scan, float *out){
+    memset(out, 0, sizeof(out));
+    out[0] = -1;
+    int temp_cnt[5] = {0}
+    for (int i = 0; i < *scan.npoints; i++){
+        if (-0.523599< scan->points[i].angle && scan->points[i]< 0.523599){ //-30 to 30
+            out[1] += scan->points[i].range;
+            temp_cnt[1]+=1;
+        } else if (1.0472 < scan->points[i].angle && scan->points[i]< 2.0944){ // 60 to 120
+            out[2] += scan->points[i].range;
+            temp_cnt[2]+=1;
+        } else if (2.61799 < scan->points[i].angle && scan->points[i]< 3.14159
+                ||-3.14159 < scan->points[i].angle && scan->points[i]< -2.61799 ){ // 150 to 180 or -180 to -150
+            out[3] += scan->points[i].range;
+            temp_cnt[3]+=1;
+        } else if (-2.0944 < scan->points[i].angle && scan->points[i]< -1.0472){ // -120 to -60
+            out[4] += scan->points[i].range;
+            temp_cnt[4]+=1;
+        }
+    }
+    for (int i = 1; i <5; i++){
+        out[i] = out[i] / temp_cnt[i];
+    }
+}
+
+
 int main(int argc, const char *argv[])
 {   int server_fd, new_socket, valread;
         struct sockaddr_in address;
@@ -161,22 +190,24 @@ int main(int argc, const char *argv[])
 
     printf("started\n");
 
-    kobukiDriveDirect(50, 50);
+    kobukiDriveDirect(60, 60);
     
     KobukiSensors_t sensors;
     sensors.leftWheelEncoder = 0;
     sensors.rightWheelEncoder = 0;
+
+    float avgs[5] = {0};
     
-    struct packet {
-      float stamp;
-      float range;
-      float angle;
-      int leftWheelEncoder;
-      int rightWheelEncoder;      
-    };
-    struct packet pkt;
-    pkt.leftWheelEncoder = 0;
-    pkt.rightWheelEncoder = 0;
+    // struct packet {
+    //   float stamp;
+    //   float range;
+    //   float angle;
+    //   int leftWheelEncoder;
+    //   int rightWheelEncoder;      
+    // };
+    // struct packet pkt;
+    // pkt.leftWheelEncoder = 0;
+    // pkt.rightWheelEncoder = 0;
     
         while (1) {
             switch (state) {
@@ -184,6 +215,7 @@ int main(int argc, const char *argv[])
                     if(ret && os_isOk()) {
                         if(doProcessSimple(laser, &scan)) {
                             kobukiSensorPoll(&sensors);
+                            getAvg(&scan, avgs); 
                             state = Send;
                         }
                     } else {
@@ -204,14 +236,12 @@ int main(int argc, const char *argv[])
                     point[4] = sensors.rightWheelEncoder;
                     
                     for (int i = 0; i < scan.npoints; i++){
-                            // printf("scans: %d\n", scan.npoints);
                             // fprintf(stdout, "distance %f angle %.4f\n", scan.points[i].range*100, scan.points[i].angle * 57.29);
                             point[1] = scan.points[i].range;
                             point[2] = scan.points[i].angle;
-                            
+                            //TODO send 4 avgs
                             // pkt.range = scan.points[i].range;
                             // pkt.angle = scan.points[i].angle;
-                            
                             // printf("stamp: %d distance: %.2f angle: %.2f ticks (%u, %u)\n", pkt.stamp, pkt.range, pkt.angle,
                                                                                                         // pkt.leftWheelEncoder, pkt.rightWheelEncoder);
                             
@@ -224,6 +254,7 @@ int main(int argc, const char *argv[])
                             // send(new_socket, &pkt, sizeof(struct packet), 0 );
                             fflush(stdout);
                         }
+                        printf("0: %f, 90: %f 180: %f 270: %f\n", avgs[1], avgs[2], avgs[3], avgs[4]);
                     state = Drive;
                     break;
 
@@ -231,7 +262,7 @@ int main(int argc, const char *argv[])
                     break;
 
                 case Drive:
-                    kobukiDriveDirect(50, 50);
+                    kobukiDriveDirect(60, 60);
                     state = Scanning;
                     break;
                 

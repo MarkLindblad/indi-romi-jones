85d9a91c5a63da6996398cde0e65e58851a7fdec
BassemHalim <bassemamir459@gmail.com>
2021-12-10 23:19:26 +0000

    Modified the state machine and implemented the Receive and Drive state

--- software/main.c
+++ software/main.c
@@ -21,7 +21,6 @@
 #endif
 #include <string.h>
 
-// #include "./YDLidar/src/ydlidar_sdk.h"
 #include "ydlidar_sdk.h"
 #if defined(_MSC_VER)
 #pragma comment(lib, "ydlidar_sdk.lib")
@@ -29,11 +28,8 @@
 
 #include "kobuki.h"
 
-// #include <fcntl.h>
-// #include <errno.h>
-// #include <termios.h>
-// #include <unistd.h>
-#define SEND false
+#define SPEED 20
+#define SEND true
 
 // get the avg distance in 4 directions, out => [-1, 0, 90, 180, 270] 
 //angles are in radian and range from -180 to 180
@@ -64,133 +60,131 @@ void getAvg(LaserFan *scan, float *out){
 }
 
 
-int main(int argc, const char *argv[])
-{   int server_fd, new_socket, valread;
-        struct sockaddr_in address;
-        int opt = 1;
-        int addrlen = sizeof(address);
-    if (SEND){
-    //--------------socket------------------------
-       
-       // perror("started");
+int main(int argc, const char *argv[]) {   
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    if (SEND) {
+        //--------------socket------------------------
+        // perror("started");
         // Creating socket file descriptor
-        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-        {
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
             perror("socket failed");
             exit(EXIT_FAILURE);
         }
 
         // Forcefully attaching socket to the port 8080
-        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                      &opt, sizeof(opt)))
-        {
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
             perror("setsockopt");
             exit(EXIT_FAILURE);
         }
+
         address.sin_family = AF_INET;
         address.sin_addr.s_addr = INADDR_ANY;
         address.sin_port = htons( PORT );
         // Forcefully attaching socket to the port 8080
-        if (bind(server_fd, (struct sockaddr *)&address,
-                                     sizeof(address))<0)
-        {
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
             perror("bind failed");
             exit(EXIT_FAILURE);
         }
-        if (listen(server_fd, 3) < 0)
-        {
+        if (listen(server_fd, 3) < 0) {
             perror("listen");
             exit(EXIT_FAILURE);
         }
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                           (socklen_t*)&addrlen))<0)
-        {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
-        }
-        //------------LIDAR init--------------------
+    }
+    //------------LIDAR init--------------------
 
-        os_init();
-        
-        YDLidar *laser = lidarCreate();
-        //string prop
-        char port[50] = "/dev/ydlidar";
-        LidarPort ports;
-        int size = lidarPortList(&ports);
-        int i = 0;
-        for(i =0; i < size; i++) {
-            printf("port: %s\n", ports.port[i].data);
-            /// last port
-            strcpy(port, ports.port[i].data);
-        }
-        setlidaropt(laser, LidarPropSerialPort, port, sizeof(port));
-        strcpy(port, "");
-        setlidaropt(laser, LidarPropIgnoreArray,port, sizeof(port));
+    os_init(); 
+    
+    YDLidar *laser = lidarCreate();
+    //string prop
+    char port[50] = "/dev/ydlidar";
+    LidarPort ports;
+    int size = lidarPortList(&ports);
+    int i = 0;
+    for(i = 0; i < size; i++) {
+        printf("port: %s\n", ports.port[i].data);
+        /// last port
+        strcpy(port, ports.port[i].data);
+    }
+    setlidaropt(laser, LidarPropSerialPort, port, sizeof(port));
+    strcpy(port, "");
+    setlidaropt(laser, LidarPropIgnoreArray,port, sizeof(port));
 
-        //int prop
-        int i_optvalue = 115200;
-        setlidaropt(laser, LidarPropSerialBaudrate, &i_optvalue, sizeof(int));
-        // i_optvalue = TYPE_TOF;
-        i_optvalue = TYPE_TRIANGLE;
-        setlidaropt(laser, LidarPropLidarType, &i_optvalue, sizeof(int));
-        i_optvalue = YDLIDAR_TYPE_SERIAL;
-        setlidaropt(laser, LidarPropDeviceType, &i_optvalue, sizeof(int));
-        i_optvalue = 3;
-        setlidaropt(laser, LidarPropSampleRate, &i_optvalue, sizeof(int));
+    //int prop
+    int i_optvalue = 115200;
+    setlidaropt(laser, LidarPropSerialBaudrate, &i_optvalue, sizeof(int));
+    // i_optvalue = TYPE_TOF;
+    i_optvalue = TYPE_TRIANGLE;
+    setlidaropt(laser, LidarPropLidarType, &i_optvalue, sizeof(int));
+    i_optvalue = YDLIDAR_TYPE_SERIAL;
+    setlidaropt(laser, LidarPropDeviceType, &i_optvalue, sizeof(int));
+    i_optvalue = 3;
+    setlidaropt(laser, LidarPropSampleRate, &i_optvalue, sizeof(int));
 
-        //bool prop
-        bool b_optval = true;
-        setlidaropt(laser, LidarPropAutoReconnect, &b_optval, sizeof(bool));
-        b_optval = true;
-        setlidaropt(laser, LidarPropSingleChannel, &b_optval, sizeof(bool));
-        b_optval = false;
-        setlidaropt(laser, LidarPropIntenstiy, &b_optval, sizeof(bool));
-        setlidaropt(laser, LidarPropInverted, &b_optval, sizeof(bool));
-        setlidaropt(laser, LidarPropReversion, &b_optval, sizeof(bool));
-        b_optval = true;
-        setlidaropt(laser, LidarPropSupportMotorDtrCtrl, &b_optval, sizeof(bool));
-        b_optval = false;
-        setlidaropt(laser, LidarPropFixedResolution, &b_optval, sizeof(bool));
+    //bool prop
+    bool b_optval = true;
+    setlidaropt(laser, LidarPropAutoReconnect, &b_optval, sizeof(bool));
+    b_optval = true;
+    setlidaropt(laser, LidarPropSingleChannel, &b_optval, sizeof(bool));
+    b_optval = false;
+    setlidaropt(laser, LidarPropIntenstiy, &b_optval, sizeof(bool));
+    setlidaropt(laser, LidarPropInverted, &b_optval, sizeof(bool));
+    setlidaropt(laser, LidarPropReversion, &b_optval, sizeof(bool));
+    b_optval = true;
+    setlidaropt(laser, LidarPropSupportMotorDtrCtrl, &b_optval, sizeof(bool));
+    b_optval = false;
+    setlidaropt(laser, LidarPropFixedResolution, &b_optval, sizeof(bool));
 
-        //float prop
-        float f_optval = 4.f;
-        setlidaropt(laser, LidarPropScanFrequency, &f_optval, sizeof(float));
-        f_optval = 180.0f;
-        setlidaropt(laser, LidarPropMaxAngle, &f_optval, sizeof(float));
-        f_optval = -180.0f;
-        setlidaropt(laser, LidarPropMinAngle, &f_optval, sizeof(float));
-        // f_optval = 8.f;
-        f_optval = 10.f;
-        setlidaropt(laser, LidarPropMaxRange, &f_optval, sizeof(float));
-        // f_optval = 0.12f;
-        f_optval = 0.12f;
-        setlidaropt(laser, LidarPropMinRange, &f_optval, sizeof(float));
+    //float prop
+    float f_optval = 4.f;
+    setlidaropt(laser, LidarPropScanFrequency, &f_optval, sizeof(float));
+    f_optval = 180.0f;
+    setlidaropt(laser, LidarPropMaxAngle, &f_optval, sizeof(float));
+    f_optval = -180.0f;
+    setlidaropt(laser, LidarPropMinAngle, &f_optval, sizeof(float));
+    // f_optval = 8.f;
+    f_optval = 10.f;
+    setlidaropt(laser, LidarPropMaxRange, &f_optval, sizeof(float));
+    // f_optval = 0.12f;
+    f_optval = 0.12f;
+    setlidaropt(laser, LidarPropMinRange, &f_optval, sizeof(float));
 
-        getlidaropt(laser, LidarPropSerialBaudrate,&i_optvalue, sizeof(int));
-        printf("baudrate: %d\n", i_optvalue);
+    getlidaropt(laser, LidarPropSerialBaudrate,&i_optvalue, sizeof(int));
+    printf("baudrate: %d\n", i_optvalue);
 
-        bool ret = initialize(laser);
-       if(ret) {
-           ret = turnOn(laser);
-       }
+    bool ret = initialize(laser);
+    if(ret) {
+        ret = turnOn(laser);
+    }
 
-        LaserFan scan;
-        LaserFanInit(&scan);
+    LaserFan scan;
+    LaserFanInit(&scan);
 
-    typedef enum{
+    typedef enum {
         Scanning,
         Send,
         Receive,
         Drive,
         Close
-    }state_t;
+    } state_t;
+
+    // 0: S (stop), 1: F (forward), 2: R (right), 3: L (left)
+    typedef enum {
+        S,
+        F,
+        R,
+        L,
+    } direction_t;
 
     state_t state = Scanning;
-
+    direction_t direction = S;
     printf("started\n");
-
-    kobukiDriveDirect(60, 60);
     
     KobukiSensors_t sensors;
     sensors.leftWheelEncoder = 0;
@@ -209,74 +203,98 @@ int main(int argc, const char *argv[])
     // pkt.leftWheelEncoder = 0;
     // pkt.rightWheelEncoder = 0;
     
-        while (1) {
-            switch (state) {
-                case Scanning:
-                    if(ret && os_isOk()) {
-                        if(doProcessSimple(laser, &scan)) {
-                            kobukiSensorPoll(&sensors);
-                            getAvg(&scan, avgs); 
-                            state = Send;
-                        }
-                    } else {
-                        fprintf(stderr, "Failed to get Lidar Data\n");
-                        fflush(stderr);
-                        state = Close;
+    while (1) {
+        switch (state) {
+            case Scanning:
+            // Romi scans the environment using the lidar
+                if(ret && os_isOk()) {
+                    if(doProcessSimple(laser, &scan)) {
+                        kobukiSensorPoll(&sensors);
+                        getAvg(&scan, avgs); 
+                        state = Send;
                     }
-                    break;
-                case Send:
-                    ;
-                    float point[5] = {0}; // timestamp, distance, angle, ticks left, ticks right
-                    // pkt.stamp = scan.stamp;
-                    // pkt.leftWheelEncoder = sensors.leftWheelEncoder;
-                    // pkt.rightWheelEncoder = sensors.rightWheelEncoder;
+                } else {
+                    fprintf(stderr, "Failed to get Lidar Data\n");
+                    fflush(stderr);
+                    state = Close;
+                }
+                break;
+            case Send: ;
+            // Romi sends lidar and wheel encoder data to central computer
+                float sensor_data[5] = {0}; // timestamp, distance, angle, ticks left, ticks right
+                // pkt.stamp = scan.stamp;
+                // pkt.leftWheelEncoder = sensors.leftWheelEncoder;
+                // pkt.rightWheelEncoder = sensors.rightWheelEncoder;
+                
+                sensor_data[0] = scan.stamp;
+                sensor_data[3] = sensors.leftWheelEncoder;
+                sensor_data[4] = sensors.rightWheelEncoder;
+                
+                for (int i = 0; i < scan.npoints; i++) {
+                    // fprintf(stdout, "distance %f angle %.4f\n", scan.points[i].range*100, scan.points[i].angle * 57.29);
+                    sensor_data[1] = scan.points[i].range;
+                    sensor_data[2] = scan.points[i].angle;
+                    // pkt.range = scan.points[i].range;
+                    // pkt.angle = scan.points[i].angle;
+                    // printf("stamp: %d distance: %.2f angle: %.2f ticks (%u, %u)\n", pkt.stamp, pkt.range, pkt.angle,
+                                                                                                // pkt.leftWheelEncoder, pkt.rightWheelEncoder);
                     
-                    point[0] = scan.stamp;
-                    point[3] = sensors.leftWheelEncoder;
-                    point[4] = sensors.rightWheelEncoder;
-                    
-                    for (int i = 0; i < scan.npoints; i++){
-                            // fprintf(stdout, "distance %f angle %.4f\n", scan.points[i].range*100, scan.points[i].angle * 57.29);
-                            point[1] = scan.points[i].range;
-                            point[2] = scan.points[i].angle;
-                            // pkt.range = scan.points[i].range;
-                            // pkt.angle = scan.points[i].angle;
-                            // printf("stamp: %d distance: %.2f angle: %.2f ticks (%u, %u)\n", pkt.stamp, pkt.range, pkt.angle,
-                                                                                                        // pkt.leftWheelEncoder, pkt.rightWheelEncoder);
-                            
-                            printf("stamp: %d distance: %.2f angle: %.2f ticks (%u, %u)\n", point[0], point[1], point[2],
-                                                                                                        point[3], point[4]);
-                            // sprintf (msg, "%.0f, %.0f",scan.points[i].range/10, scan.points[i].angle * 57.29 );
-                            if (SEND){
-                                send(new_socket , point , 5 * sizeof(float) , 0 );
-                            }
-                            // send(new_socket, &pkt, sizeof(struct packet), 0 );
-                            fflush(stdout);
+                    //printf("stamp: %d", point[3]);
+                    // sprintf (msg, "%.0f, %.0f",scan.points[i].range/10, scan.points[i].angle * 57.29 );
+                    if (SEND) {
+                        if (send(new_socket, sensor_data, 5 * sizeof(float), 0) == -1) {
+                            perror("send failed");
+                            exit(EXIT_FAILURE);
                         }
-                        printf("0: %f, 90: %f 180: %f 270: %f\n", avgs[1]*100, avgs[2]*100, avgs[3]*100, avgs[4]*100); 
-                        if (SEND){
-                            send(new_socket , avgs , 5 * sizeof(float) , 0 );
-                        }
-                        
-
+                    }
+                    // send(new_socket, &pkt, sizeof(struct packet), 0 );
+                    fflush(stdout);
+                }
+                //printf("0: %f, 90: %f 180: %f 270: %f\n", avgs[1]*100, avgs[2]*100, avgs[3]*100, avgs[4]*100); 
+                if (SEND) {
+                //    send(new_socket , avgs , 5 * sizeof(float) , 0 );
+                }
+                state = Receive;
+                break;
+            case Receive: ;
+            // Romi receives a direction from the central computer
+                //TODO receive direction
+                // Pointer to char
+                char* dir;
+                *dir = 'A';
+                if (recv(new_socket, &dir, sizeof(char), 0) == -1) {
+                    perror("receive failed");
+                    exit(EXIT_FAILURE);
+                } else {
+                    printf("Direction: %c \n", *dir);
+                    direction = *dir;
                     state = Drive;
                     break;
+                }
 
-                case Receive:
-                    break;
-
-                case Drive:
-                    kobukiDriveDirect(0, 0);
-                    state = Scanning;
-                    break;
+            case Drive:
+            // Romi moves in the direction given by the central computer
+                switch(direction) {
+                    case S:
+                        kobukiDriveDirect(0, 0);
+                    case F:
+                        kobukiDriveDirect(75, 75);
+                    case R:
+                        kobukiDriveDirect(75, -75);
+                    case L:
+                        kobukiDriveDirect(-75, 75);
+                }
+                state = Scanning;
+                break;
                 
-                case Close:
-                    kobukiDriveDirect(0,0);
-                    LaserFanDestroy(&scan);
-                    turnOff(laser);
-                    disconnecting(laser);
-                    lidarDestroy(&laser);
-                    return 0;
-            }
-        }        
+            
+            case Close:
+                kobukiDriveDirect(0,0);
+                LaserFanDestroy(&scan);
+                turnOff(laser);
+                disconnecting(laser);
+                lidarDestroy(&laser);
+                return 0;
+        }
+    }       
 }

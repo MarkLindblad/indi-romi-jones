9a0820ce23892e3775ab9c03fc6c42b493ca7397
BassemHalim <bassemamir459@gmail.com>
2021-12-16 09:27:22 +0000

    before merge

--- build/romi_robots/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/williamcatzin/indi_workspace/src/romi_robots
-# Build directory: /home/williamcatzin/indi_workspace/build/romi_robots
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
